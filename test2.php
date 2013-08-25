d9e58d22d455f3d2f9efe10eab92084e06469832
apostoloskritikos <akritiko@gmail.com>
2013-08-25 11:38:12 +0300

    Update?

--- test2.php
+++ test2.php
@@ -2,7 +2,7 @@
 require_once(__DIR__.'/config.php');
 require_once(__DIR__.'/functions.php');
 
-$path = getAllShows();
+$path = getAllShows(); 
 $csv = readCSV($path, "title");
 printCSV($csv, 'List of TV Series in Epguides');
 
